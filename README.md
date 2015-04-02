1570f4b9da1015a60b02f336df52c1f49a38c1ca
Mostafa Razavi <mostafa@sepent.com>
2015-04-02 22:33:32 +0430

    The project was renamed to bttools. btparser added.
    A BitTorrent parser (btparser) was added to the project. The project
    is now called bttools.

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 # Overview
 
+bttools is a set of Python tools for experimenting with the BitTorrent
+protocol. It current consists of two tools: utptrace and btparser.
+
+# utptrace
+
 uTP (uTorrent Transport Protocol, also known as the Micro Transport
 Protocol) is a reliable transport protocol implemented over UDP. It is
 used by some BitTorrent clients for file transfer. See [BEP 29][1] for
 more information.
 
 py-utptrace is a Python library for tracing uTP streams. At the moment
-it works as a simple script that receives a PCAP file and traces it
-(the reassembled stream is not really written anywhere!). This will
-hopefully change very soon.
+it works as a simple script that receives a PCAP file, traces it and
+writes the assembled streams to disk.
+
+# btparser
+
+btparser parses the BitTorrent streams. The output of utptrace can be
+passed to btparser as an argument. btparser then prints out what it
+finds in the given stream.
 
 [1]: http://www.bittorrent.org/beps/bep_0029.html
---
+++ btparser.py
@@ -0,0 +1,189 @@
+#!/usr/bin/env python
+
+import bencode
+
+import sys
+import struct
+from socket import ntohl, ntohs
+
+message_parsers = {}
+extended_message_parsers = {}
+
+# maps extended message numbers to their names
+extended_message_associtions = {}
+
+def register_message(n):
+    def decorator(f):
+        def wrapper(*args, **kwargs):
+            f(*args, **kwargs)
+        message_parsers[n] = f
+        return wrapper
+    return decorator
+
+def register_extended_message(n):
+    def decorator(f):
+        def wrapper(*args, **kwargs):
+            f(*args, **kwargs)
+        extended_message_parsers[n] = f
+        return wrapper
+    return decorator
+
+@register_message(0)
+def parse_message_choke(stream, n, length):
+    print 'CHOKE'
+
+@register_message(1)
+def parse_message_choke(stream, n, length):
+    print 'UNCHOKE'
+
+@register_message(2)
+def parse_message_choke(stream, n, length):
+    print 'INTERESTED'
+
+@register_message(3)
+def parse_message_choke(stream, n, length):
+    print 'NOT INTERESTED'
+
+@register_message(4)
+def parse_message_choke(stream, n, length):
+    index = ntohl(struct.unpack('I', stream[n:n+4])[0])
+    print 'HAVE:', index
+
+@register_message(5)
+def parse_message_choke(stream, n, length):
+    print 'BITFIELD:', ''.join(bin(ord(i))[2:] for i in stream[n:n + length - 5])
+
+@register_message(6)
+def parse_message_choke(stream, n, length):
+    if n + 11 > len(stream):
+        print 'Unexpected end of stream.'
+        exit(1)
+    index = ntohl(struct.unpack('I', stream[n:n+4])[0])
+    begin = ntohl(struct.unpack('I', stream[n+4:n+8])[0])
+    length = ntohl(struct.unpack('I', stream[n+8:n+12])[0])
+    print 'REQUEST: index={} begin={} length={}'.format(index, begin, length)
+
+@register_message(7)
+def parse_message_piece(stream, n, length):
+    index = ntohl(struct.unpack('I', stream[n:n+4])[0])
+    begin = ntohl(struct.unpack('I', stream[n+4:n+8])[0])
+    print 'PIECE: index={} begin={} block_size={}'.format(index, begin, length - 5 - 4)
+
+@register_message(8)
+def parse_message_choke(stream, n, length):
+    index = ntohl(struct.unpack('I', stream[n:n+4])[0])
+    begin = ntohl(struct.unpack('I', stream[n+4:n+8])[0])
+    length = ntohl(struct.unpack('I', stream[n+8:n+12])[0])
+    print 'CANCEL: index={} begin={} length={}'.format(index, begin, length)
+
+@register_message(9)
+def parse_message_port(stream, n, length):
+    port = ntohs(struct.unpack('H', stream[n:n+2])[0])
+    print 'DHT Port:', port
+
+@register_message(0x0d)
+def parse_message_have_all(stream, n, length):
+    index = ntohl(struct.unpack('I', stream[n:n+4])[0])
+    print 'SUGGEST PIECE:', index
+
+@register_message(0x0e)
+def parse_message_have_all(stream, n, length):
+    print 'HAVE ALL'
+
+@register_message(0x0f)
+def parse_message_have_all(stream, n, length):
+    print 'HAVE NONE'
+
+@register_message(0x10)
+def parse_message_have_all(stream, n, length):
+    index = ntohl(struct.unpack('I', stream[n:n+4])[0])
+    begin = ntohl(struct.unpack('I', stream[n+4:n+8])[0])
+    length = ntohl(struct.unpack('I', stream[n+8:n+12])[0])
+    print 'REJECT:', index, begin, length
+
+@register_message(0x11)
+def parse_message_have_all(stream, n, length):
+    index = ntohl(struct.unpack('I', stream[n:n+4])[0])
+    print 'ALLOWED FAST:', index
+
+@register_message(20)
+def parser_message_extended(stream, n, length):
+    id = ord(stream[n])
+    n += 1
+    if id == 0:
+        handshake = stream[n:n + (length - 2)]
+        handshake = bencode.bdecode(handshake)
+        print 'EXTENDED: Handshake:', handshake
+
+        for name, number in handshake['m'].items():
+            if number == 0:
+                # disable this extension
+                extended_message_associations = {
+                    k: v for k, v in extended_message_associtions.items()
+                    if v != name}
+            else:
+                # add this extension
+                extended_message_associtions[number] = name
+    elif id in extended_message_associtions:
+        name = extended_message_associtions[id]
+        if name not in extended_message_parsers:
+            print 'EXTENDED: UNKNOWN EXTENSION PROTOCOL:', name
+            return
+        extended_message_parsers[name](stream[1:], n, length - 1)
+    else:
+        print 'EXTENDED: UNKNOWN MESSAGE ID:', id
+        return
+
+@register_extended_message('foo')
+def parse_message_foo(stream, n, length):
+    pass
+
+def parse_message(stream, n):
+    length = ntohl(struct.unpack('I', stream[n:n + 4])[0])
+    if length == 0:
+        return n + 4
+
+    id = ord(stream[n + 4])
+
+    try:
+        message_parsers[id](stream, n + 5, length)
+    except KeyError as e:
+        print 'UNKNOWN MESSAGE ID:', id
+
+    return n + length + 4
+
+def parse_stream(stream):
+    pstrlen = ord(stream[0])
+    pstr = stream[1:pstrlen + 1]
+
+    if pstr != 'BitTorrent protocol':
+        print 'Stream does not contain BitTorrent data.'
+        exit(1)
+
+    print 'pstr:', pstr
+
+    n = 1 + pstrlen
+    reserved = stream[n:n + 8]
+    print 'reserved:', `reserved`
+    n += 8
+
+    infohash = stream[n:n + 20]
+    print 'infohash:', `infohash`
+    n += 20
+
+    peerid = stream[n:n + 20]
+    print 'peerid:', `peerid`
+    n += 20
+
+    while n < len(stream):
+        n = parse_message(stream, n)
+
+def main():
+    filename = sys.argv[1]
+    with open(filename) as f:
+        stream = f.read()
+
+    parse_stream(stream)
+
+if __name__ == '__main__':
+    main()
