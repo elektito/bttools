78ae78f473431a8d06d4ca48f9be65ea4527716f
Mostafa Razavi <mostafa@sepent.com>
2015-04-05 23:29:39 +0430

    Re-structured btparser.py into a BitTorrentParser class.
    The newly added BitTorrentParser class is similar to the recently
    added UtpTracer. It is event based and also uses logging.

--- btparser.py
+++ btparser.py
@@ -2,9 +2,23 @@
 
 import bencode
 
+import logging
 import sys
 import struct
 from socket import ntohl, ntohs
+
+class BitTorrentParserError(Exception):
+    pass
+
+class UnexpectedEndOfStreamError(BitTorrentParserError):
+    def __init__(self):
+        super(UnexpectedEndOfStreamError, self).__init__('Unexpected end of stream.')
+
+class InvalidBitTorrentStreamError(BitTorrentParserError):
+    def __init__(self, msg='Invalid BitTorrent stream.'):
+        super(InvalidBitTorrentStreamError, self).__init__(msg)
+
+EXT_ALLOWED_FAST = 1
 
 message_parsers = {}
 extended_message_parsers = {}
@@ -28,184 +42,262 @@ def register_extended_message(n):
         return wrapper
     return decorator
 
-@register_message(0)
-def parse_message_choke(stream, n, length):
-    print 'CHOKE'
+class BitTorrentParser(object):
+    def __init__(self):
+        self.infos = {}
 
-@register_message(1)
-def parse_message_choke(stream, n, length):
-    print 'UNCHOKE'
+        self.logger = logging.getLogger('utptrace')
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler(sys.stdout)
+        self.logger.addHandler(handler)
 
-@register_message(2)
-def parse_message_choke(stream, n, length):
-    print 'INTERESTED'
+    def add_info(self, info):
+        self.infos[str(hashlib.sha1(bencode.bencode(info)))] = info
 
-@register_message(3)
-def parse_message_choke(stream, n, length):
-    print 'NOT INTERESTED'
+    def parse_stream(self, stream):
+        pstrlen = ord(stream[0])
+        pstr = stream[1:pstrlen + 1]
 
-@register_message(4)
-def parse_message_choke(stream, n, length):
-    index = ntohl(struct.unpack('I', stream[n+5:n+9])[0])
-    print 'HAVE:', index
+        if pstr != 'BitTorrent protocol':
+            msg = 'Stream does not contain BitTorrent data.'
+            self.logger.error(msg)
+            raise InvalidBitTorrentStreamError(msg)
 
-@register_message(5)
-def parse_message_choke(stream, n, length):
-    print 'BITFIELD:', ''.join(bin(ord(i))[2:] for i in stream[n+5:n+length])
+        self.logger.info('pstr: {}'.format(pstr))
 
-@register_message(6)
-def parse_message_choke(stream, n, length):
-    if n + 17 >= len(stream):
-        print 'Unexpected end of stream.'
-        exit(1)
-    index = ntohl(struct.unpack('I', stream[n+5:n+9])[0])
-    begin = ntohl(struct.unpack('I', stream[n+9:n+13])[0])
-    length = ntohl(struct.unpack('I', stream[n+13:n+17])[0])
-    print 'REQUEST: index={} begin={} length={}'.format(index, begin, length)
+        n = 1 + pstrlen
+        reserved = stream[n:n + 8]
+        self.logger.info('reserved: {}'.format(reserved.encode('hex')))
+        n += 8
 
-@register_message(7)
-def parse_message_piece(stream, n, length):
-    index = ntohl(struct.unpack('I', stream[n+5:n+9])[0])
-    begin = ntohl(struct.unpack('I', stream[n+9:n+13])[0])
-    print 'PIECE: index={} begin={} block_size={}'.format(index, begin, length - 1 - 8)
+        infohash = stream[n:n + 20]
+        self.logger.info('infohash: {}'.format(infohash.encode('hex')))
+        n += 20
 
-@register_message(8)
-def parse_message_choke(stream, n, length):
-    index = ntohl(struct.unpack('I', stream[n+5:n+9])[0])
-    begin = ntohl(struct.unpack('I', stream[n+9:n+13])[0])
-    length = ntohl(struct.unpack('I', stream[n+13:n+17])[0])
-    print 'CANCEL: index={} begin={} length={}'.format(index, begin, length)
+        peerid = stream[n:n + 20]
+        self.logger.info('peerid: {}'.format(peerid.encode('hex')))
+        n += 20
 
-@register_message(9)
-def parse_message_port(stream, n, length):
-    port = ntohs(struct.unpack('H', stream[n+5:n+7])[0])
-    print 'DHT Port:', port
+        while n < len(stream):
+            n = self.parse_message(stream, n)
 
-@register_message(0x0d)
-def parse_message_suggest_piece(stream, n, length):
-    index = ntohl(struct.unpack('I', stream[n+5:n+9])[0])
-    print 'SUGGEST PIECE:', index
+    def parse_message(self, stream, n):
+        length = ntohl(struct.unpack('I', stream[n:n + 4])[0])
+        if length == 0:
+            return n + 4
 
-@register_message(0x0e)
-def parse_message_have_all(stream, n, length):
-    print 'HAVE ALL'
+        id = ord(stream[n + 4])
 
-@register_message(0x0f)
-def parse_message_have_none(stream, n, length):
-    print 'HAVE NONE'
+        try:
+            message_parsers[id](self, stream, n, length)
+        except KeyError as e:
+            print 'UNKNOWN MESSAGE ID:', id
 
-@register_message(0x10)
-def parse_message_reject(stream, n, length):
-    index = ntohl(struct.unpack('I', stream[n+5:n+9])[0])
-    begin = ntohl(struct.unpack('I', stream[n+9:n+13])[0])
-    length = ntohl(struct.unpack('I', stream[n+13:n+17])[0])
-    print 'REJECT:', index, begin, length
+        return n + length + 4
 
-@register_message(0x11)
-def parse_message_allowed_fast(stream, n, length):
-    index = ntohl(struct.unpack('I', stream[n+5:n+9])[0])
-    print 'ALLOWED FAST:', index
+    @register_extended_message('upload_only')
+    def parse_message_upload_only(self, stream, n, length):
+        payload = stream[n+6:n+length+4]
+        self.logger.info('[MESSAGE] [EXTENDED] upload_only: turned {}'.format(
+            'off' if payload[0] == '\x00' else 'on'))
+        self.__new_extended_message('upload_only', value=(payload[0] != '\x00'))
 
-@register_message(20)
-def parser_message_extended(stream, n, length):
-    n += 5
-    id = ord(stream[n])
-    n += 1
-    if id == 0:
-        handshake = stream[n:n + (length - 2)]
-        handshake = bencode.bdecode(handshake)
-        print 'EXTENDED: Handshake:', handshake
+    @register_extended_message('lt_tex')
+    def parse_message_lt_pex(self, stream, n, length):
+        lt_tex = bencode.bdecode(stream[n+6:n+length+4])
+        self.logger.info('[MESSAGE] [EXTENDED] lt_tex: announced {} tracker(s).'.format(
+            len(lt_tex['added'])))
+        self.__new_extended_message('lt_pex', added=lt_tex['added'])
 
-        for name, number in handshake['m'].items():
-            if number == 0:
-                # disable this extension
-                extended_message_associations = {
-                    k: v for k, v in extended_message_associtions.items()
-                    if v != name}
-            else:
-                # add this extension
-                extended_message_associtions[number] = name
-    elif id in extended_message_associtions:
-        name = extended_message_associtions[id]
-        if name not in extended_message_parsers:
-            print 'EXTENDED: UNKNOWN EXTENSION PROTOCOL:', name
+    @register_extended_message('ut_pex')
+    def parse_message_ut_pex(self, stream, n, length):
+        ut_pex = bencode.bdecode(stream[n+6:n+length+4])
+        added = ut_pex['added']
+        prefer_encryption = len([i for i in ut_pex['added.f'] if ord(i) & 0x01 == 1])
+        seeders = len([i for i in ut_pex['added.f'] if ord(i) & 0x02 == 1])
+        self.logger.info(
+            '[MESSAGE] [EXTENDED] ut_pex: added {} peers ({} prefer(s) '
+            'encryption; {} is/are seeder(s)). dropped {}.'.format(
+                len(added),
+                prefer_encryption,
+                seeders,
+                len(ut_pex['dropped'])))
+
+        if all(k in ut_pex for k in ['added6', 'added6.f', 'dropped6']) and \
+           (len(ut_pex['added6']) > 0 or len(ut_pex['dropped6']) > 0):
+            added = ut_pex['added6']
+            prefer_encryption = len([i for i in ut_pex['added6.f'] if ord(i) & 0x01 == 1])
+            seeders = len([i for i in ut_pex['added6.f'] if ord(i) & 0x02 == 1])
+            self.logger.info(
+                '[MESSAGE] [EXTENDED]         also added {} IPv6 peers '
+                '({} prefer(s) encryption; {} is/are seeder(s)). '
+                'dropped {}.'.format(
+                    len(added),
+                    prefer_encryption,
+                    seeders,
+                    len(ut_pex['dropped6'])))
+
+        self.__new_extended_message('ut_pex', value=ut_pex)
+
+    @register_message(20)
+    def parser_message_extended(self, stream, n, length):
+        n += 5
+        id = ord(stream[n])
+        n += 1
+        if id == 0:
+            handshake = stream[n:n + (length - 2)]
+            handshake = bencode.bdecode(handshake)
+            self.logger.info('[MESSAGE] [EXTENDED] HANDSHAKE: {}'.format(handshake))
+
+            for name, number in handshake['m'].items():
+                if number == 0:
+                    # disable this extension
+                    extended_message_associations = {
+                        k: v for k, v in extended_message_associtions.items()
+                        if v != name}
+                else:
+                    # add this extension
+                    extended_message_associtions[number] = name
+        elif id in extended_message_associtions:
+            name = extended_message_associtions[id]
+            if name not in extended_message_parsers:
+                self.new_extended_message(name)
+                return
+            extended_message_parsers[name](self, stream, n - 6, length)
+        else:
+            print 'EXTENDED: UNKNOWN MESSAGE ID:', id
             return
-        extended_message_parsers[name](stream, n - 6, length)
-    else:
-        print 'EXTENDED: UNKNOWN MESSAGE ID:', id
-        return
 
-@register_extended_message('lt_tex')
-def parse_message_foo(stream, n, length):
-    lt_tex = bencode.bdecode(stream[n+6:n+length+4])
-    print 'lt_tex: announced {} tracker(s).'.format(len(lt_tex['added']))
+    @register_message(0)
+    def parse_message_choke(self, stream, n, length):
+        self.logger.info('[MESSAGE] CHOKE')
+        self.__new_message('choke')
 
-@register_extended_message('ut_pex')
-def parse_message_foo(stream, n, length):
-    ut_pex = bencode.bdecode(stream[n+6:n+length+4])
-    added = ut_pex['added']
-    prefer_encryption = len([i for i in ut_pex['added.f'] if ord(i) & 0x01 == 1])
-    seeders = len([i for i in ut_pex['added.f'] if ord(i) & 0x02 == 1])
-    print 'ut_pex: added {} peers ({} prefer(s) encryption; {} is/are seeder(s)). dropped {}.'.format(len(added), prefer_encryption, seeders, len(ut_pex['dropped']))
+    @register_message(1)
+    def parse_message_unchoke(self, stream, n, length):
+        self.logger.info('[MESSAGE] UNCHOKE')
+        self.__new_message('unchoke')
 
-    if all(k in ut_pex for k in ['added6', 'added6.f', 'dropped6']) and \
-       (len(ut_pex['added6']) > 0 or len(ut_pex['dropped6']) > 0):
-        added = ut_pex['added6']
-        prefer_encryption = len([i for i in ut_pex['added6.f'] if ord(i) & 0x01 == 1])
-        seeders = len([i for i in ut_pex['added6.f'] if ord(i) & 0x02 == 1])
-        print '        also added {} IPv6 peers ({} prefer(s) encryption; {} is/are seeder(s)). dropped {}.'.format(len(added), prefer_encryption, seeders, len(ut_pex['dropped6']))
+    @register_message(2)
+    def parse_message_interested(self, stream, n, length):
+        self.logger.info('[MESSAGE] INTERESTED')
+        self.__new_message('interested')
 
-@register_extended_message('upload_only')
-def parse_message_foo(stream, n, length):
-    payload = stream[n+6:n+length+4]
-    print 'upload_only: turned', 'off' if payload[0] == '\x00' else 'on'
+    @register_message(3)
+    def parse_message_not_interested(self, stream, n, length):
+        self.logger.info('[MESSAGE] NOT INTERESTED')
+        self.__new_message('not_interested')
 
-def parse_message(stream, n):
-    length = ntohl(struct.unpack('I', stream[n:n + 4])[0])
-    if length == 0:
-        return n + 4
+    @register_message(4)
+    def parse_message_have(self, stream, n, length):
+        index = ntohl(struct.unpack('I', stream[n+5:n+9])[0])
+        self.logger.info('[MESSAGE] HAVE: {}'.format(index))
+        self.__new_message('have', index=index)
 
-    id = ord(stream[n + 4])
+    @register_message(5)
+    def parse_message_bitfield(self, stream, n, length):
+        bitfield = stream[n+5:n+length]
+        bitfield_str = ''.join(bin(ord(i))[2:] for i in stream[n+5:n+length])
+        self.logger.info('[MESSAGE] BITFIELD: {}'.format(bitfield_str))
+        self.__new_message('bitfield', bitfield=bitfield)
 
-    try:
-        message_parsers[id](stream, n, length)
-    except KeyError as e:
-        print 'UNKNOWN MESSAGE ID:', id
+    @register_message(6)
+    def parse_message_request(self, stream, n, length):
+        if n + 17 >= len(stream):
+            raise UnexpectedEndOfStreamError()
+        index = ntohl(struct.unpack('I', stream[n+5:n+9])[0])
+        begin = ntohl(struct.unpack('I', stream[n+9:n+13])[0])
+        length = ntohl(struct.unpack('I', stream[n+13:n+17])[0])
+        self.logger.info(
+            '[MESSAGE] REQUEST: index={} begin={} length={}'.format(
+                index, begin, length))
+        self.__new_message('request', index=index, begin=begin, length=length)
 
-    return n + length + 4
+    @register_message(7)
+    def parse_message_piece(self, stream, n, length):
+        index = ntohl(struct.unpack('I', stream[n+5:n+9])[0])
+        begin = ntohl(struct.unpack('I', stream[n+9:n+13])[0])
+        block_size = length - 1 - 8
+        self.logger.info(
+            '[MESSAGE] PIECE: index={} begin={} length={}'.format(
+                index, begin, block_size))
+        self.__new_message('piece', index=index, begin=begin, length=block_size)
 
-def parse_stream(stream):
-    pstrlen = ord(stream[0])
-    pstr = stream[1:pstrlen + 1]
+    @register_message(8)
+    def parse_message_choke(self, stream, n, length):
+        index = ntohl(struct.unpack('I', stream[n+5:n+9])[0])
+        begin = ntohl(struct.unpack('I', stream[n+9:n+13])[0])
+        length = ntohl(struct.unpack('I', stream[n+13:n+17])[0])
+        self.logger.info(
+            '[MESSAGE] CANCEL: index={} begin={} length={}'.format(
+                index, begin, length))
+        self.__new_message('cancel', index=index, begin=begin, length=length)
 
-    if pstr != 'BitTorrent protocol':
-        print 'Stream does not contain BitTorrent data.'
-        exit(1)
+    @register_message(9)
+    def parse_message_port(self, stream, n, length):
+        port = ntohs(struct.unpack('H', stream[n+5:n+7])[0])
+        self.logger.info('[MESSAGE] PORT: {}'.format(port))
+        self.__new_message('port', port=port)
 
-    print 'pstr:', pstr
+    @register_message(0x0d)
+    def parse_message_suggest_piece(self, stream, n, length):
+        index = ntohl(struct.unpack('I', stream[n+5:n+9])[0])
+        self.logger.info('[MESSAGE] SUGGEST PIECE: {}'.format(port))
+        self.__new_message('suggest_piece', index=index)
 
-    n = 1 + pstrlen
-    reserved = stream[n:n + 8]
-    print 'reserved:', `reserved`
-    n += 8
+    @register_message(0x0e)
+    def parse_message_have_all(self, stream, n, length):
+        self.logger.info('[MESSAGE] HAVE ALL')
+        self.__new_message('have_all')
 
-    infohash = stream[n:n + 20]
-    print 'infohash:', `infohash`
-    n += 20
+    @register_message(0x0f)
+    def parse_message_have_none(self, stream, n, length):
+        self.logger.info('[MESSAGE] HAVE NONE')
+        self.__new_message('have_none')
 
-    peerid = stream[n:n + 20]
-    print 'peerid:', `peerid`
-    n += 20
+    @register_message(0x10)
+    def parse_message_reject(self, stream, n, length):
+        index = ntohl(struct.unpack('I', stream[n+5:n+9])[0])
+        begin = ntohl(struct.unpack('I', stream[n+9:n+13])[0])
+        length = ntohl(struct.unpack('I', stream[n+13:n+17])[0])
+        self.logger.info(
+            '[MESSAGE] REJECT: index={} begin={} length={}'.format(
+                index, begin, length))
+        self.__new_message('reject')
 
-    while n < len(stream):
-        n = parse_message(stream, n)
+    @register_message(0x11)
+    def parse_message_allowed_fast(self, stream, n, length):
+        index = ntohl(struct.unpack('I', stream[n+5:n+9])[0])
+        self.logger.info('[MESSAGE] ALLOWED FAST: {}'.format(index))
+        self.__new_message('allowed_fast')
+
+    def __new_extended_message(self, name, **attrs):
+        self.__new_message(20, extension_name=name, **attrs)
+        self.new_extended_message(name, **attrs)
+
+    def __new_message(self, name, **attrs):
+        self.new_message(name, **attrs)
+
+    def new_message(self, name, **attrs):
+        pass
+
+    def new_extended_message(self, name, **attrs):
+        pass
+
+class MyBitTorrentParser(BitTorrentParser):
+    pass
 
 def main():
     filename = sys.argv[1]
     with open(filename) as f:
         stream = f.read()
 
-    parse_stream(stream)
+    parser = MyBitTorrentParser()
+
+    try:
+        parser.parse_stream(stream)
+    except BitTorrentParserError as e:
+        print 'Error:', e
 
 if __name__ == '__main__':
     main()
